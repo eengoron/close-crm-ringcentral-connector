3c992c291dc6962e1370d169a515689b4d6043d8
Eric Engoron <eric.engoron@gmail.com>
2020-01-13 09:44:52 -0500

    Remove need to ping anything

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, session
 from .methods import find_and_post_rc_calls_to_close, refresh_rc_platform
 from apscheduler.schedulers.background import BackgroundScheduler
-import requests
 import logging
 import os
 
@@ -19,16 +18,11 @@ def job1():
 
 def job2():
     refresh_rc_platform()
-    
-def job3():
-    requests.get(os.environ.get('PING_URL'))
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(job1,"interval", seconds=int(os.environ.get('seconds')))
 scheduler.add_job(job2, "interval", seconds=3400)
-scheduler.add_job(job3, "interval", seconds=1740)
 scheduler.start()
 app = Flask(__name__)
-from app import routes
 if __name__ == '__main__':
     app.run(use_reloader=False, debug=True)
--- app/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, redirect, url_for, flash, request
-from app import app
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html',  title='Close <> RingCentral Connector')
